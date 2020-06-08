c22c2647794df51cc0c972b9eddb168b8cf5458e
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-06-07 21:42:06 -0500

    Validando errorres al abrir imagen, los controles btn aplicarcolor, sliderr, sliderg, y sliderb se desactiban hasta que se habra una nueva imagen

---
+++ MATLAB/UNIDAD 2/EDITOR DE COLOR/Editor_de_color.m
@@ -0,0 +1,213 @@
+%Creacion de interfaz con menus, que permiten
+%agregar una imgen y colocarle el color con una
+%slider que regula el color, que posteriormente
+%se guardara la imagen dependiendo de las configuraciones
+%colacadas en el slider
+
+function varargout = Editor_de_color(varargin)
+% EDITOR_DE_COLOR MATLAB code for Editor_de_color.fig
+%      EDITOR_DE_COLOR, by itself, creates a new EDITOR_DE_COLOR or raises the existing
+%      singleton*.
+%
+%      H = EDITOR_DE_COLOR returns the handle to a new EDITOR_DE_COLOR or the handle to
+%      the existing singleton*.
+%
+%      EDITOR_DE_COLOR('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EDITOR_DE_COLOR.M with the given input arguments.
+%
+%      EDITOR_DE_COLOR('Property','Value',...) creates a new EDITOR_DE_COLOR or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Editor_de_color_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Editor_de_color_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Editor_de_color
+
+% Last Modified by GUIDE v2.5 07-Jun-2020 21:13:58
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Editor_de_color_OpeningFcn, ...
+                   'gui_OutputFcn',  @Editor_de_color_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Editor_de_color is made visible.
+function Editor_de_color_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Editor_de_color (see VARARGIN)
+
+% Choose default command line output for Editor_de_color
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Editor_de_color wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Editor_de_color_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --------------------------------------------------------------------
+function mnuopciones_Callback(hObject, eventdata, handles)
+% hObject    handle to mnuopciones (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+ set(handles.mnunuevo, 'enable','off'); %Desabilita el boton nuevo
+ set(handles.mnuguardar, 'enable','off'); %Desabilita el boton guardar
+ set(handles.sliderr, 'enable','off');
+ set(handles.sliderg, 'enable','off');
+ set(handles.sliderb, 'enable','off');
+ set(handles.btnaplicarcolor, 'enable','off');
+% --------------------------------------------------------------------
+function mnuabrir_Callback(hObject, eventdata, handles)
+% hObject    handle to mnuabrir (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global a;
+
+[Filename Path]=uigetfile({'*.jpg';'*.png'},'Abrir imagen');
+if isequal(Filename,0)
+   errordlg('Error no se ha seleccionado archivo', 'Error');
+else
+   
+    a=imread(strcat(Path,Filename));
+    imshow(a);
+    msgbox('Se abrio imagen con exito', 'Abrir imagen');
+     %Menu de opciones configuraciones
+    set(handles.mnunuevo, 'enable','on'); %Habilita el boton nuevo
+    set(handles.mnuguardar, 'enable','on'); %Habilita el boton guardar
+    set(handles.mnuabrir,'enable','off');   %Desabilita el boton abrir
+    set(handles.btnaplicarcolor, 'enable','on');
+    set(handles.sliderr, 'enable','on');
+    set(handles.sliderg, 'enable','on');
+    set(handles.sliderb, 'enable','on');
+end
+% --------------------------------------------------------------------
+function mnunuevo_Callback(hObject, eventdata, handles)
+% hObject    handle to mnunuevo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function mnuguardar_Callback(hObject, eventdata, handles)
+% hObject    handle to mnuguardar (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function mnusalir_Callback(hObject, eventdata, handles)
+op=questdlg('Desea salir de la aplicacion','Salir','Si','No','Si');
+if (op=='Si')
+    delete(handles.figure1);
+end
+
+
+% --- Executes on button press in btnaplicarcolor.
+function btnaplicarcolor_Callback(hObject, eventdata, handles)
+% hObject    handle to btnaplicarcolor (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on slider movement.
+function sliderr_Callback(hObject, eventdata, handles)
+%A las variables r,g y b obtiene el valor del slider y almacena.
+r=get(handles.sliderr,'Value');
+g=get(handles.sliderg,'Value');
+b=get(handles.sliderb,'Value');
+%A las etiguetas de r,g y b se le asigna el valor obtenido de los slider
+set(handles.lblnumr,'String',r);
+set(handles.panelcolor,'BackgroundColor',[r g b]);
+
+
+% --- Executes during object creation, after setting all properties.
+function sliderr_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sliderr (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function sliderg_Callback(hObject, eventdata, handles)
+r=get(handles.sliderr,'Value');
+g=get(handles.sliderg,'Value');
+b=get(handles.sliderb,'Value');
+set(handles.lblnumg,'String',g);
+set(handles.panelcolor,'BackgroundColor',[r g b]);
+
+% --- Executes during object creation, after setting all properties.
+function sliderg_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sliderg (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes on slider movement.
+function sliderb_Callback(hObject, eventdata, handles)
+r=get(handles.sliderr,'Value');
+g=get(handles.sliderg,'Value');
+b=get(handles.sliderb,'Value');
+set(handles.lblnumb,'String',b);
+set(handles.panelcolor,'BackgroundColor',[r g b]);
+
+% --- Executes during object creation, after setting all properties.
+function sliderb_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sliderb (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function lblnumr_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to lblnumr (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
